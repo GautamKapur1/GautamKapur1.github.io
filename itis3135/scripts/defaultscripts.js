f38962f2dead0fdd8fec3743cd35075980348df5
GautamKapur1 <gautkapur@gmail.com>
2022-02-14 18:18:32 -0500

    fixing bug

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -37,7 +37,6 @@ function calcCost()
     let kiwiAmnt = document.getElementById("kiwiAmnt").value;
     let cost = kiwiCost * kiwiAmnt;
     let message = "The total cost is: $" + parseFloat(cost.toFixed(2));
-    calcTax(cost);
     document.getElementById("calculateCost").innerHTML = message;
 }
 
