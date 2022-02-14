b1e24bb85998c0ece76e7dfb8497de0b479bf86c
GautamKapur1 <gautkapur@gmail.com>
2022-02-14 17:56:47 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -40,10 +40,13 @@ function calcCost()
     document.getElementById("calculateCost").innerHTML = message;
 }
 
-function calcTax(cost)
+function calcTax()
 {
     let kiwiTax = document.getElementById("kiwiTax").value;
+    let kiwiCost = document.getElementById("kiwiCost").value;
+    let kiwiAmnt = document.getElementById("kiwiAmnt").value;
     kiwiTax /= 100;
+    let cost = kiwiCost * kiwiAmnt;
     let costAfterTax = cost - (cost * kiwiTax);
     let message = "The total cost after tax is: $" + parseFloat(costAfterTax.toFixed(2));
     document.getElementById("calculateTax").innerHTML = message;
