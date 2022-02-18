0d346fc618f6b806c3ea924ca7261a90389461a5
GautamKapur1 <gautkapur@gmail.com>
2022-02-17 23:54:51 -0500

    added a polygon namer

--- itis3135/scripts/polygonnamer.js
+++ itis3135/scripts/polygonnamer.js
@@ -1,4 +1,46 @@
 function alertUser()
 {
     var guess = prompt("Please enter a number between 1-10 and the name of the polygon will be printed!");
+    validateNum(guess);
+}
+
+function validateNum(_guess)
+{
+    if(isNaN(_guess)){
+        alert("Sorry! That was not a number between 1-10! Please try again!");
+        alertUser();
+    }
+    else
+    {
+        number = Math.trunc(_guess);
+        if(number > 10 || number < 1)
+        {
+            alert("Sorry! That was not a number between 1-10! Please try again!");
+            alertUser();
+        }
+        else
+        {
+            polygonName(number);
+        }
+    }
+}
+
+function polygonName(sides)
+{
+    var polygonList = new Array(
+        "Henagon", 
+        "Digon", 
+        "Triangle", 
+        "Quadrilateral", 
+        "Pentagon", 
+        "Hexagon", 
+        "Heptagon", 
+        "Octagon",
+        "Nonagon",
+        "Decagon",  
+    );
+    
+    var message = "A polygon with " + sides + " sides is called a " + polygonList[sides - 1] + ".";
+
+    document.getElementById("polygonName").innerHTML = message;
 }
