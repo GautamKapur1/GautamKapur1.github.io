7a6c5029359b06ef729138f5298b257a84c395f9
GautamKapur1 <gautkapur@gmail.com>
2022-02-17 23:33:01 -0500

    Update polygonnamer.js

--- itis3135/scripts/polygonnamer.js
+++ itis3135/scripts/polygonnamer.js
@@ -1,4 +1,4 @@
 function alertUser()
 {
-    alert("Please enter a number between 1-10 and the name of the polygon will be printed!");
+    var guess = prompt("Please enter a number between 1-10 and the name of the polygon will be printed!");
 }
