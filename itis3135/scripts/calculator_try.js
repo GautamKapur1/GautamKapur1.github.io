26cd40cf3657e19662b46ca017e39b9fcea4d9b9
GautamKapur1 <gautkapur@gmail.com>
2022-02-26 16:09:58 -0500

    trying calculator

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,20 @@
+clearButton()
+{
+    var calcOutput = "0";
+    document.getElementById("calcOutput").innerHTML = calcOutput;
+}
+
+absValueButton()
+{
+
+}
+
+percentButton()
+{
+
+}
+
+divisionButton()
+{
+
+}
