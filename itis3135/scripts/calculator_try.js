c90e82e9a9b1bcb0234e2fa981bc706865295c3e
GautamKapur1 <gautkapur@gmail.com>
2022-02-26 16:21:46 -0500

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -20,12 +20,14 @@ function divisionButton()
 
 function sevenButton()
 {
+    console.log(parseInt(document.getElementById("calcLabel")) == 0);
     if (parseInt(document.getElementById("calcLabel")) == 0)
     {
         document.getElementById("calcLabel").innerHTML = 7;
     }
     else 
     {
+        
         document.getElementById("calcLabel").innerHTML += 7;
     }
 }
