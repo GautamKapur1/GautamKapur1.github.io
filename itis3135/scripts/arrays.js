ebc024c090cf772845a5bec8ce0d1e90d56fde3c
GautamKapur1 <gautkapur@gmail.com>
2022-03-06 14:44:10 -0500

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,9 +1,18 @@
 let people = new Array("Gautam Kapur", "Michael Jackson", "Bill Murray", "Keanu Reeves");
 let salaries = new Array(130000, 91000, 62000, 283000);
 
+//loop through salary array and get average salary. display highest salary (and person) also
 function displayResults()
 {
+    let totalSalary = 0;
+    let avg = 0;
 
+    for (i = 0; i < salaries.length; i++)
+    {
+        totalSalary += salaries[i];
+    }
+
+    avg = totalSalary / salaries.length;
 }
 
 function displaySalary()
