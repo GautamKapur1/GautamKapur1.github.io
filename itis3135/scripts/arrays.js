73f2c4a5b4dd7e1248b2adfcd092856c1cc4f608
GautamKapur1 <gautkapur@gmail.com>
2022-03-06 17:18:48 -0500

    Update arrays.js

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -42,6 +42,6 @@ function displaySalary()
 
 function addSalary()
 {
-    let person = document.getElementById("employeeList").nodeValue;
+    let person = document.getElementById("employeeList").value;
     console.log(person);
 }
