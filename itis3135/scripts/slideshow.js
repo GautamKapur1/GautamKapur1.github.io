435469143297106d07efcdec9110a1e8dccbdfed
GautamKapur1 <gautkapur@gmail.com>
2022-03-26 18:24:22 -0400

    fixed scripts

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,16 +1,22 @@
 $(document).ready(function() 
 {
     var imgLink;
+    var curr_pic;
+    var next_pic;
 
     //when left arrow clicked
     $('#leftarrow').on('click', function()
     {
-
+        curr_pic = $('#curr_pic');
+        next_pic = curr_pic.next();
+        curr_pic.removeAttr('id');
+        next_pic.attr('id', 'curr_pic')
     });
 
     //when right arrow clicked
     $('#rightarrow').on('click', function()
     {
-        
+        curr_pic = $('#curr_pic');
+
     });
 })
