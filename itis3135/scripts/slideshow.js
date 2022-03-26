67102e911086baa9049e80c2777b38e423d5b999
GautamKapur1 <gautkapur@gmail.com>
2022-03-26 18:15:29 -0400

    Update slideshow.js

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,16 @@
+$(document).ready(function() 
+{
+    var imgLink;
+
+    //when left arrow clicked
+    $('#leftarrow').on('click', function()
+    {
+
+    });
+
+    //when right arrow clicked
+    $('#rightarrow').on('click', function()
+    {
+        
+    });
+})
