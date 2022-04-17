7f307f17af2c5decbdf471421ac39f9de19578b1
GautamKapur1 <gautkapur@gmail.com>
2022-04-17 16:33:59 -0400

    activity 14 files fixed

--- itis3135/activity14/kapur-activity14.js
+++ itis3135/activity14/kapur-activity14.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
-	
+	$("#nav_list li").click(function(){
+        var title = $(this).children("a").attr("title");
+
+        //use same ajax method as activity 13 to get info but replace team with main
+        //also replace url with json_files/[title].json
+
+        $.ajax({
+            type: "get",
+            url: "json_files/" + title + ".json",
+            beforeSend: function() {
+                $("main").html("Loading...");
+            },
+            timeout: 10000,
+            error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function(data) {
+                $("main").html("");
+    
+                $.each(data,function(){
+                    $.each(this, function(key, value){
+                        $("main").append(
+                            "<h1>" + value.title + "</h1>" + 
+                            "<h2>" + value.month + "</h2>" + 
+                            "<h3>" + value.speaker + "</h3>" +
+                            "<img src = " + value.image + " alt = " + title + "_picture >" + 
+                            "<p>" + value.text + "</p>" 
+                        )
+                    })
+                })
+                
+            }
+        });
+    });
 }); // end ready
