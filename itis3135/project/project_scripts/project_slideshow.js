4e552fa336c5240c9d55d92f0ac9fbf35cfc74f6
GautamKapur1 <gautkapur@gmail.com>
2022-04-26 22:25:15 -0400

    updated my bxslider for project

---
+++ itis3135/project/project_scripts/project_slideshow.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      minSlides: 1,
+      maxSlides: 1,
+      slideWidth: 700,
+      adaptiveHeight: true,
+      captions: true,
+      speed: 1000,
+      auto: true,
+      moveSlides: 1
+    });
+});
