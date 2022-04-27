20854bed2ea24c2b113983e7ebfeadf16857f792
GautamKapur1 <gautkapur@gmail.com>
2022-04-26 23:04:18 -0400

    Update project_slideshow.js

--- itis3135/project/project_scripts/project_slideshow.js
+++ itis3135/project/project_scripts/project_slideshow.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     $('.slider').bxSlider({
-        randomStart: true,
         moveSlides: 1,
-        auto: true,
-        pause: 3000,
         minSlides: 1,
         maxSlides: 1,
         slideWidth: 500,
-        slideMargin: 20
+        slideMargin: 20,
+        captions: true
     });
 });
