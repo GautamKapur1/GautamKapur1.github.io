c62538e08bdb4ab1fe9a2e77d3435eda1ec197c3
GautamKapur1 <gautkapur@gmail.com>
2022-05-08 18:34:23 -0400

    added to the js for my form in project

--- itis3135/project/project_scripts/project_classes.js
+++ itis3135/project/project_scripts/project_classes.js
@@ -3,9 +3,12 @@ function calcCost()
     var classesPerWeek = document.getElementById("classes_per_week");
     var classLength = document.getElementById("class_length");
     var displayCost = document.getElementById("cost");
-    let count = 0;
-    let costPerHrOfClass = 15;
+    var personAge = document.getElementById("person_age");
+    var option = personAge.options[personAge.selectedIndex];
 
+    var costPerHrOfClass = 0;
+    var count = 0;
+    
     displayCost.innerHTML = "";
 
     if (classesPerWeek.value == 0 || classesPerWeek.value < 1 || classesPerWeek.value > 7 || classesPerWeek.value % 1 != 0) 
@@ -19,6 +22,10 @@ function calcCost()
         {
             displayCost.innerHTML += "<p><strong>Error</strong>: Please enter a valid class length (anywhere between 30 minutes and 3 hours).</p>";
         }
+        if (option.value == 0) 
+        {
+            displayCost.innerHTML += "<p><strong>Error</strong>: Please select the swimmer's age range.</p>";
+        }
     }
     else
     {
@@ -26,13 +33,36 @@ function calcCost()
         {
             displayCost.innerHTML += "<strong>Error</strong>: Please enter a valid class length (anywhere between 30 minutes and 3 hours).";
         }
+        if (option.value == 0) 
+        {
+            displayCost.innerHTML += "<strong>Error</strong>: Please select the swimmer's age range.";
+        }
+
     }
     
-    if (classesPerWeek.value != 0 && classLength.value != 0) 
+    if (classesPerWeek.value != 0 && classLength.value != 0 && option.value != 0) 
     {
-        let hrsClassPerWeek = classesPerWeek.value * classLength.value;
-        let cost = hrsClassPerWeek * costPerHrOfClass;
-        displayCost.innerHTML += "<strong>Total Cost: $" + cost.toFixed(2) + "</strong>";
-        //15$ per hour of class
+        //$7.49 per hour of class for child
+        //$12.49 per hour of class for young adult
+        //$14.99 per hour of class for adult
+
+        if (option.value == "child") 
+        {
+            costPerHrOfClass = 7.49;
+        }
+        else if (option.value == "young_adult")
+        {
+            costPerHrOfClass = 12.49;
+        }
+        else if (option.value == "adult")
+        {
+            costPerHrOfClass = 14.99;
+        }
+
+        var hrsClassPerWeek = classesPerWeek.value * classLength.value;
+        var cost = hrsClassPerWeek * costPerHrOfClass;
+        displayCost.innerHTML += "The cost for 1 hour of class is $" + costPerHrOfClass.toFixed(2) + 
+        "<p><strong>Total Cost: $" + cost.toFixed(2) + "</strong></p>";
+
     }
 }
