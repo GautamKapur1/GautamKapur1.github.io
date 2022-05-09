e8c7eaaadbc4814325092722a2ed5c144f58fd07
GautamKapur1 <gautkapur@gmail.com>
2022-05-09 18:49:39 -0400

    added buttons

---
+++ itis3135/project/project_scripts/project_basics.js
@@ -0,0 +1,19 @@
+function fstyleVid()
+{
+    window.open("https://www.youtube.com/watch?v=AQy_c30lNjI", "_blank");
+}
+
+function bkstrokeVid()
+{
+    window.open("https://www.youtube.com/watch?v=MrFt6JHii8w", "_blank");
+}
+
+function btstrokeVid()
+{
+    window.open("https://www.youtube.com/watch?v=EElzlIMjk_c", "_blank");
+}
+
+function bflyVid()
+{
+    window.open("https://www.youtube.com/watch?v=riIyImmuB_M", "_blank");
+}
